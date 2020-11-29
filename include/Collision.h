6ef3f376f2df51562c4923a0da70f7a3c7306224
tspt89 <tspt89@hotmail.com>
2020-11-29 13:04:43 -0600

    Documentacion de Collision

--- include/Collision.h
+++ include/Collision.h
@@ -9,7 +9,13 @@ class Collision
 	public:
 		Collision();
 		virtual ~Collision();
+		/*Funcion que recibe como parametros coordenadas en el plano XZ del jugador  del agente,
+		* para que se determine la distancia entre ambos, y se pueda ver si entra dentro del radio
+		* de colision */
 		bool isColliding(float,float,float,float,float);
+		/* Funcion para determinar si el agente o jugador se encuentra dentro del escenario,
+		en caso de que no, sus coordenadas se asignan unos pixeles atras para que no se salga
+		y se le impida avanzar.*/
 		bool isCollidingWall(float*,float*,float*,float);
 	protected:
 
--- src/Collision.cpp
+++ src/Collision.cpp
@@ -10,25 +10,26 @@ Collision::~Collision()
 	//dtor
 }
 
+/*Funcion que recibe como parametros coordenadas en el plano XZ del jugador  del agente,
+* para que se determine la distancia entre ambos, y se pueda ver si entra dentro del radio
+* de colision */
 bool Collision::isColliding(float pX, float pZ, float aX, float aZ, float radius){
-	//printf("Player: %3.2f\t%3.2f\t%3.2f\t\tAgent: %3.2f\t%3.2f\t%3.2f\n",player[0],player[1],player[2],agent[0],agent[1],agent[2]);
-	float distance = sqrt(pow((pX - aX),2) + pow((pZ - aZ),2));
-	//printf("RADIUS: %f (r = %3.2f)\tX: %3.2f\tY: %3.2f\tZ: %3.2f\tDistance:%3.2f\t-->%s\n",radius,100,pX,0.0f,pZ,distance,(distance <= radius) ? "true":"false");
-	//usleep(750000);
-	//printf("Player = (%3.2f, %3.2f, %3.f) Agent = (%3.2f, %3.2f, %3.2f) - rad = %3.2f - dist = %3.2f\t%s\n",pX,0.0f,pZ,aX,0.0f,aZ,radius,distance,(distance <= radius) ? "true":"false");
-	//usleep(1000000);
-	return (distance <= radius);
+	float distance = sqrt(pow((pX - aX),2) + pow((pZ - aZ),2)); //Se saca la distancia entre dos puntos del player y del agente
+
+	return (distance <= radius); //Return de un bool si la distancia es menor al radio.
 }
 
+/* Funcion para determinar si el agente o jugador se encuentra dentro del escenario,
+	en caso de que no, sus coordenadas se asignan unos pixeles atras para que no se salga
+	y se le impida avanzar.*/
 bool Collision::isCollidingWall(float *pX, float *pY, float *pZ, float l){
-	bool var = false;
-	var = ((*pX > l) || (*pZ > l) || (*pX<= -l) || (*pZ<= -l));
+	bool var = ((*pX > l) || (*pZ > l) || (*pX<= -l) || (*pZ<= -l));
+
 	*pX= (*pX > l)? l - (0.07f * l) : *pX;
 	*pZ= (*pZ > l)? l - (0.07f * l) : *pZ;
 	*pX= (*pX<= -l) ? -l + (0.07f * l): *pX;
 	*pZ= (*pZ<= -l) ? -l + (0.07f * l): *pZ;
 
-	//printf("---- %3.2f - %3.2f - %3.2f\t\t\tIs colliding with walls: %s\n",*pX,*pY,*pZ,(var)?"true":"false");
-	//usleep(300000);
+
 	return var;
 }
