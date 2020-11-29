50d1deccf632620e1598b2f847d34b82141c3e70
tspt89 <tspt89@hotmail.com>
2020-11-29 13:41:15 -0600

    Documentacion de Player

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,6 +2,7 @@
 
 Player::Player()
 {
+	//Asignacion inicial del usuario para iniciar la perspectiva
 	EYE_X=0.0;
     EYE_Y=0.0;
     EYE_Z=0.0;
@@ -21,6 +22,7 @@ Player::~Player()
 	//dtor
 }
 
+//Funcion para avanzar
 void Player::up(){
 	EYE_X += Direction[0];
 	EYE_Y += Direction[1];
@@ -30,6 +32,7 @@ void Player::up(){
 	CENTER_Z = EYE_Z + Direction[2];
 }
 
+//Funcion para retroceder
 void Player::down(){
 	EYE_X -= Direction[0];
 	EYE_Y -= Direction[1];
@@ -39,18 +42,21 @@ void Player::down(){
 	CENTER_Z = EYE_Z + Direction[2];
 }
 
+//Funcion para girar hacia la derecha
 void Player::right(){
 	Theta += dTheta;
 	Theta = (Theta > 359.0) ? 0.0 : Theta;
 	this->lookAt();
 }
 
+//Funcion para girar hacia la izquierda
 void Player::left(){
 	Theta -= dTheta;
 	Theta = (Theta < 0.0) ? 359.0 : Theta;
 	this->lookAt();
 }
 
+//Funcion para avanzar de acuerdo al angulo que se obtiene
 void Player::lookAt(){
 	Direction[0] = cos(DegToRad(Theta));
     Direction[2] = sin(DegToRad(Theta));
@@ -58,11 +64,13 @@ void Player::lookAt(){
     CENTER_Z = EYE_Z + Direction[2];
 }
 
+//Funcion para covertir grados a radianes
 float Player::DegToRad(float g)
 {
       return ((g*M_PI)/180.0);
 }
 
+//Funcion para atualizar la perspectiva del usuario
 void Player::updatePerspective(){
 	glLoadIdentity();
     gluLookAt(EYE_X,EYE_Y,EYE_Z,CENTER_X,CENTER_Y,CENTER_Z,UP_X,UP_Y,UP_Z);
